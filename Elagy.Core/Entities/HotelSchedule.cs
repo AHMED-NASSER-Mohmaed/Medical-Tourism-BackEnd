73b1d707b08254891de16937b6a2bf10df6a00e7
Mohamed Elmahdy <engmohamedelmadhy936@gmail.com>
2025-07-03 23:08:43 +0300

    last update

--- Elagy.Core/Entities/HotelSchedule.cs
+++ Elagy.Core/Entities/HotelSchedule.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Elagy.Core.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,6 +14,7 @@ namespace Elagy.Core.Entities
         public decimal TotalPrice { get; set;}
         public  DateOnly StartDate { get; set; }
         public DateOnly EndDate { get; set; }
+        public PayStatus RoomscheduleStatus { get; set; } = PayStatus.Confirmed;
         public int RoomId { get; set; }
         public Room Room { get; set; }
     }
---
+++ Elagy.Core/Enums/PayStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elagy.Core.Enums
+{
+ 
+    public enum PayStatus
+    {
+        Pending=0, Confirmed=1, Cancelled=2,
+    }
+}
