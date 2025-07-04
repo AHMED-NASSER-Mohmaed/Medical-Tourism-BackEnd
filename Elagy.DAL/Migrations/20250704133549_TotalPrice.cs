14730540de82dda2caf89bb156a4f39395db73de
Ahmed Nasser <ahmed.nasser.elgendy0@gmail.com>
2025-07-04 16:36:56 +0300

    Migration AddedToDB

---
+++ Elagy.DAL/Migrations/20250704133549_TotalPrice.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Elagy.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class TotalPrice : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Price",
+                table: "RoomSchedule",
+                newName: "TotalPrice");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Price",
+                table: "Cars",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "TotalPrice",
+                table: "CarRentalSchedules",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Cars");
+
+            migrationBuilder.DropColumn(
+                name: "TotalPrice",
+                table: "CarRentalSchedules");
+
+            migrationBuilder.RenameColumn(
+                name: "TotalPrice",
+                table: "RoomSchedule",
+                newName: "Price");
+        }
+    }
+}
