2e6281b82161ca48d8523b0549996d88bd5284ef
Mohamed Elmahdy <engmohamedelmadhy936@gmail.com>
2025-07-12 17:37:51 +0300

    merge last updates

--- Elagy.Core/Entities/HotelAsset .cs
+++ Elagy.Core/Entities/HotelAsset .cs
@@ -5,7 +5,6 @@
         public int? StarRating { get; set; } // E.g., 1 to 5 stars
         public bool? HasPool { get; set; }
         public bool? HasRestaurant { get; set; }
-
         public ICollection<Room>? Rooms { get; set; }
         public ICollection<HotelAssetImage>? HotelAssetImages { get; set; }
     }
